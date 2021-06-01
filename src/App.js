9ad5bd8c6221d79f6a7e83a602f1e2bc948faed1
Carlos Botero-Vargas <80601385+Carlos-BoteroVargas@users.noreply.github.com>
2021-05-31 21:02:25 -0500

    Initial commit, 6 different color styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import './css/switcher.css';
 
 function App() {
+  // state definition
+  const [colorTheme, setColorTheme] = useState('theme-white');
+
+  // effect (side effect)
+  useEffect(() => {
+    // Check for selected (stored) theme in local Storage
+    const currentThemeColor = localStorage.getItem('theme-color');
+    // if found set selected theme value in stat
+    if (currentThemeColor) {
+      setColorTheme(currentThemeColor);
+    };
+  }, []);
+
+  // set theme
+  const handleClick = (theme) => {
+    setColorTheme(theme);
+    localStorage.setItem('theme-color', theme)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={`App ${colorTheme}`}>
+      <div className='theme-options'>
+        <div id="theme-white"
+          onClick={() => handleClick("theme-white")}
+          className={`${colorTheme === 'theme-white' 
+            ? 'active' : ''}`}  
+        />
+        <div id="theme-blue"
+          onClick={() => handleClick("theme-blue")}
+          className={`${colorTheme === 'theme-blue' 
+            ? 'active' : ''}`}  
+        />
+        <div id="theme-orange"
+          onClick={() => handleClick("theme-orange")}
+          className={`${colorTheme === 'theme-orange' 
+            ? 'active' : ''}`}  
+        />
+        <div id="theme-purple"
+          onClick={() => handleClick("theme-purple")}
+          className={`${colorTheme === 'theme-purple' 
+            ? 'active' : ''}`}  
+        />
+        <div id="theme-green"
+          onClick={() => handleClick("theme-green")}
+          className={`${colorTheme === 'theme-green' 
+            ? 'active' : ''}`}  
+        />
+        <div id="theme-black"
+          onClick={() => handleClick("theme-black")}
+          className={`${colorTheme === 'theme-black' 
+            ? 'active' : ''}`}  
+        />
+      </div>
+      <div className="content-box">
+        <h3>Multiple Themes Switcher/ React</h3>
+        <h5>(ReactJS, SCSS/CSS, Local Storage)</h5>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>-- Carlos Botero-Vargas</p>
+      </div>
     </div>
   );
 }
